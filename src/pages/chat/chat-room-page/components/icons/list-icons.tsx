afb5cb0deb3d3ff8ade6609bf82ed878f84e0d8e
SSA <shakibb200@gmail.com>
2024-04-06 15:00:38 +0530

    error solved

--- src/pages/chat/chat-room-page/components/icons/list-icons.tsx
+++ src/pages/chat/chat-room-page/components/icons/list-icons.tsx
@@ -1,6 +1,6 @@
 import { Svg } from "./styles";
 
-const icons = {
+const icons:any = {
   attach: (props:any) => (
     <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" {...props}>
       <path
