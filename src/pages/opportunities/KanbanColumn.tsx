1fa2697b13c96891a067c73f60c603ce87dce658
SSA <shakibb200@gmail.com>
2024-04-17 17:07:44 +0530

    coloring added to board

--- src/pages/opportunities/KanbanColumn.tsx
+++ src/pages/opportunities/KanbanColumn.tsx
@@ -11,8 +11,10 @@ const Container = styled.div`
 const Title = styled.h3`
   padding: 8px;
 `;
-const TaskList = styled.div`
+const TaskList = styled.div<{ isDraggingOver: boolean }>`
   padding: 8px;
+  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
+
 `;
 
 export const KanbanColumn = (props: any) => {
@@ -20,8 +22,12 @@ export const KanbanColumn = (props: any) => {
     <Container>
       <Title>{props.column?.title}</Title>
       <Droppable droppableId={props.column?.id}>
-        {(provided) => (
-          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+        {(provided, snapshot) => (
+          <TaskList 
+            ref={provided.innerRef} 
+            {...provided.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
+            >
             {props.tasks?.map((opp: any, index: any) => (
               <Task key={opp.id} task={opp} index={index} />
             ))}
--- src/pages/opportunities/Task.tsx
+++ src/pages/opportunities/Task.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import { Draggable } from 'react-beautiful-dnd';
 
-const Container = styled.div`
+const Container = styled.div<{ isDragging: boolean }>`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: white;
+  background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
 `;
 
 export const Task = (props: any) => {
   // return {<Container>{props.task.name}</Container>}
   return (
     <Draggable draggableId={props.task.id} index={props.index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Container
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
         >
           {props.task.name}
         </Container>
